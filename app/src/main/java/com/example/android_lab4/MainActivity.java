69d391765e569c16dbacac15bfd25a34f0f04b97
wniemiec <wiktoria.niemiec97@gmail.com>
2020-04-16 21:00:45 +0200

    Revert "listview"
    This reverts commit 2316564c5e28fc252122638a614ee51b235142a9.

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.*;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
