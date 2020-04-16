3293eb012a9fc3cef7b8d77745639ffb8ee30dfb
wniemiec <wiktoria.niemiec97@gmail.com>
2020-04-16 21:36:00 +0200

    Revert "form service"
    This reverts commit 2316564c5e28fc252122638a614ee51b235142a9.

--- app/src/main/java/com/example/android_lab4/DodajWpis.java
+++ app/src/main/java/com/example/android_lab4/DodajWpis.java
@@ -2,7 +2,10 @@ package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -11,4 +14,19 @@ public class DodajWpis extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
     }
+
+
+    public void wyslij (View view)
+    {
+        EditText kontrolka =
+                (EditText)findViewById(R.id.editText);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -42,5 +42,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
 
 }
