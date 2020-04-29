6d0341d94156f0d4735f0dde089d714519ab15c6
wniemiec <wiktoria.niemiec97@gmail.com>
2020-04-29 18:57:57 +0200

    Revert "lab05"
    This reverts commit 2316564c5e28fc252122638a614ee51b235142a9.

---
+++ app/src/main/java/com/example/android_lab4/Animal.java
@@ -0,0 +1,57 @@
+package com.example.android_lab4;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable
+{
+    private int id;
+    private  String gatunek;
+    private String kolor;
+    private float wielkosc;
+    private String opis;
+
+
+    public Animal()
+    { }
+
+    public Animal(String gatunek, String kolor, float wielkosc, String opis)
+    {
+        super();
+
+        this.gatunek = gatunek;
+        this.kolor = kolor;
+        this.wielkosc = wielkosc;
+        this.opis = opis;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Zwierze: [id=" + id + ", gatunek=" + gatunek + ", kolor=" + kolor + " wielkosc="+wielkosc+" ]";
+    }
+
+    public float getWielkosc() {
+        return wielkosc;
+    }
+
+    public String getGatunek() {
+        return gatunek;
+    }
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
